19e7f49f9e37e49ddaf01719b1e478791df5c6ec
dophucanh1082006-stack <dophucanh1082006@gmail.com>
2026-04-17 22:05:16 +0700

    Add files via upload

--- cart.js
+++ cart.js
@@ -7,11 +7,11 @@ function updateCart() {
     cartTotal = 0; // Reset về 0 mỗi lần tính lại
 
     for (let i = 0; i < prices.length; i++) {
-        let priceValue = parseFloat(prices[i].innerText.replace("$", ""));
+        let priceValue = parseFloat(prices[i].innerText.replace("đ", ""));
         let quantityValue = parseInt(quantities[i].value) || 0; // Thêm || 0 để tránh lỗi khi ô trống
         let subtotal = priceValue * quantityValue;
         
-        totals[i].innerText = "$" + subtotal.toFixed(2);
+        totals[i].innerText = subtotal + "đ";
         cartTotal += subtotal;
     }
     
@@ -21,8 +21,8 @@ function updateCart() {
 
 // Hàm này chuyên trách việc đưa con số lên giao diện HTML
 function renderTotal() {
-    document.querySelector("#subtotal table tr:nth-child(1) td:last-child").innerText = "$" + cartTotal.toFixed(2);
-    document.querySelector("#subtotal table tr:nth-child(3) td:last-child").innerText = "$" + cartTotal.toFixed(2);
+    document.querySelector("#subtotal table tr:nth-child(1) td:last-child").innerText = cartTotal.toFixed(0)+"đ";
+    document.querySelector("#subtotal table tr:nth-child(3) td:last-child").innerText = cartTotal.toFixed(0)+"đ";
 }
 
 function applyCoupon() { // Đổi tên hàm để tránh trùng với tên biến // Lấy đúng thẻ input
@@ -46,30 +46,3 @@ function checkout() {
 for (let i = 0; i < quantities.length; i++) {
     quantities[i].addEventListener("input", updateCart);
 }
-function addToCartDirect(imgId, nameId, priceId) {
-                const image = document.getElementById(imgId).src;
-                const name = document.getElementById(nameId).innerText;
-                const price = document.getElementById(priceId).innerText;
-                const size = "Default"; // Trang chủ không chọn được size, đặt mặc định
-                const qty = 1; // Nhấn vào icon ngoài trang chủ thì mặc định thêm 1 cái
-
-                const productItem = {
-                    name: name,
-                    price: price,
-                    size: size,
-                    quantity: qty,
-                    image: image
-                };
-
-                let cart = JSON.parse(localStorage.getItem('cart')) || [];
-                const existingProductIndex = cart.findIndex(item => item.name === name && item.size === size);
-
-                if (existingProductIndex !== -1) {
-                    cart[existingProductIndex].quantity += qty;
-                } else {
-                    cart.push(productItem);
-                }
-
-                localStorage.setItem('cart', JSON.stringify(cart));
-                alert('Đã thêm 1 sản phẩm ' + name + ' vào giỏ hàng!');
-            }
